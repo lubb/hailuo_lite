3b83100a67bc7331b46b37afb0d8390b34e0964b
lubingbing <414107871@qq.com>
2019-07-13 02:22:42 +0800

    处理提交拍照问题

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -20,9 +20,13 @@ export default class Index extends Component {
   constructor () {
     super(...arguments)
     this.state = {
+      defaultImg:require('./../../common/images/no_img.png'),
       order:{},
-      isArr: false,
-      isEmp: false,
+      isArr: true,
+      isEmp: true,
+      picGroup:1,
+      picGroupOrderHeavy:1,
+      picGroupOrderNull:1,
       heavy:[{
         pic0:require('./../../common/images/no_img.png'),
         pic1:require('./../../common/images/no_img.png'),
@@ -87,16 +91,20 @@ export default class Index extends Component {
      });
   }
 
-  //获取单号的图片信息
+  /**
+   * 获取单号的图片信息
+   */
   getPic(orderId, token){
-    debugger;
     let data = 'orderId='+orderId;
     ajax.postToken("/api/order/listOrderPic",data,'application/x-www-form-urlencoded', token).then(r=>{
-      console.log(r);
       let d = r.data.bizContent;
       if(d!=null && d.length>0){
-        let h =[];
-        let n =[];
+        let flagHeavy = true;
+        let numHeavy =0;
+        let picGroupOrderHeavy =1;
+        let flagNull = true;
+        let numNull =0;
+        let picGroupOrderNull =1;
         let arrayHeavy = [];
         let arrayNull = [];
         for(let i=0,len=d.length;i<len;i++){
@@ -109,30 +117,146 @@ export default class Index extends Component {
              arrayNull.push(pic);
            }
         }
-        console.log(arrayHeavy);
-        console.log(arrayNull);
         if(arrayHeavy && arrayHeavy.length>0){
-          h = this.genterData(arrayHeavy);
+          let h = this.genterData(arrayHeavy);
+          debugger
+          flagHeavy = h.flag;
+          numHeavy = h.num;
+          picGroupOrderHeavy = h.picGroupOrder;
           this.setState({
-            heavy:h
+            heavy:h.data,
+            isArr:flagHeavy,
+            picGroup:numHeavy,
+            picGroupOrderHeavy:picGroupOrderHeavy
+          })
+        }else{
+          this.setState({
+            isArr:false
           })
         }
         if(arrayNull && arrayNull.length>0){
-          n = this.genterData(arrayNull);
+          let n = this.genterData(arrayNull);
+          flagNull = n.flag;
+          numNull = n.num;
+          picGroupOrderNull = n.picGroupOrder;
+          if(numHeavy == numNull){
+            this.setState({
+              empty:n.data,
+              isEmp:flagHeavy?(flagNull?true:false):true,
+              picGroupOrderNull:picGroupOrderNull
+            })
+          }else{
+            //需要加一组空的照片
+            n.data.push({
+              pic0:this.state.defaultImg,
+              pic1:this.state.defaultImg,
+              pic2:this.state.defaultImg,
+              pic3:this.state.defaultImg
+            })
+            this.setState({
+              empty:n.data,
+              isEmp:flagHeavy?false:true,
+              picGroupOrderNull:picGroupOrderNull
+            })
+          }
+        }else{
           this.setState({
-            empty:n
+            isEmp:flagHeavy?false:true
           })
         }
       }
     });
   }
 
-  genterData(arrayHeavy){
-    var map = {},
-        dest = [],
-        result=[];
-    for(var i = 0; i < arrayHeavy.length; i++){
-      var ai = arrayHeavy[i];
+  /**
+   * 数据处理
+   * @param list
+   */
+  genterData(list){
+    let result ={};
+    let result_=[];
+    let flag_ = true;
+    let dest = this.groupByPicGroup(list);
+    let groupNum = dest.length;
+    let picGroupOrder =0;
+    if(dest && groupNum>0){
+      dest.forEach(data=>{
+        let list = data.data;
+        let len = list.length;//data数组的长度
+        picGroupOrder = len==4?1:len+1;
+        if(len!=4){
+          //表示只上传部分图片
+          flag_ = false;
+        }else{
+          //表示全部都有图片
+          flag_ = true;
+        }
+        let j = {};
+        list.forEach(d=>{
+          if(len ==1){
+            if(d.picGroupOrder==1){
+              j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            j.pic1 = this.state.defaultImg;
+            j.pic2 = this.state.defaultImg;
+            j.pic3 = this.state.defaultImg;
+          }
+          if(len ==2){
+            if(d.picGroupOrder==1){
+              j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==2){
+              j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            j.pic2 = this.state.defaultImg;
+            j.pic3 = this.state.defaultImg;
+          }
+          if(len ==3){
+            if(d.picGroupOrder==1){
+              j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==2){
+              j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==3){
+              j.pic2 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            j.pic3 = this.state.defaultImg;
+          }
+          if(len ==4){
+            if(d.picGroupOrder==1){
+              j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==2){
+              j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==3){
+              j.pic2 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+            if(d.picGroupOrder==4){
+              j.pic3 = hytApi.hailuo_img_path+'/'+d.pic;
+            }
+          }
+        })
+        result_.push(j);
+        result.flag = flag_;
+        result.data = result_;
+        result.num = groupNum;
+        result.picGroupOrder = picGroupOrder;
+      })
+    }
+    return result;
+  }
+
+  /**
+   * 将数据按照picGroup进行分组
+   * [{picGroup:1,data:[{},{},{},{}]},{picGroup:2,data:[{},{},{},{}]}]
+   * @param list
+   */
+  groupByPicGroup(list){
+    let map = {},dest = [];
+    for(let i = 0; i < list.length; i++){
+      let ai = list[i];
       if(!map[ai.picGroup]){
         dest.push({
           picGroup: ai.picGroup,
@@ -140,80 +264,20 @@ export default class Index extends Component {
         });
         map[ai.picGroup] = ai.picGroup;
       }else{
-        for(var j = 0; j < dest.length; j++){
-          var dj = dest[j];
+        for(let j = 0; j < dest.length; j++){
+          let dj = dest[j];
           if(dj.picGroup == ai.picGroup){
-            dj.data.push(ai);
+            dj.data.push(ai)
             break;
           }
         }
       }
     }
-    dest.forEach(data=>{
-      let j = {};
-      let list = data.data;
-      let len = list.length;
-      list.forEach(d=>{
-        if(len ==1){
-          if(d.picGroupOrder==1){
-            j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          j.pic1 = require('./../../common/images/no_img.png');
-          j.pic2 = require('./../../common/images/no_img.png');
-          j.pic3 = require('./../../common/images/no_img.png');
-        }
-        if(len ==2){
-          if(d.picGroupOrder==1){
-            j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==2){
-            j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          j.pic2 = require('./../../common/images/no_img.png');
-          j.pic3 = require('./../../common/images/no_img.png');
-        }
-        if(len ==3){
-          if(d.picGroupOrder==1){
-            j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==2){
-            j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==3){
-            j.pic2 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          j.pic3 = require('./../../common/images/no_img.png');
-        }
-        if(len ==4){
-          if(d.picGroupOrder==1){
-            j.pic0 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==2){
-            j.pic1 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==3){
-            j.pic2 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-          if(d.picGroupOrder==4){
-            j.pic3 = hytApi.hailuo_img_path+'/'+d.pic;
-          }
-        }
-
-
-        if(d.picGroupOrder==3){
-          j.pic2 = hytApi.hailuo_img_path+'/'+d.pic;
-        }
-        if(d.picGroupOrder==4){
-          j.pic3 = hytApi.hailuo_img_path+'/'+d.pic;
-        }
-      })
-      result.push(j);
-    })
-    return result;
+    return dest;
   }
 
   /**
-   * 获取两个月内的订单
+   * 获取一个月内的订单
    */
   getThreeMonthDay(){
     var date = new Date();
@@ -224,7 +288,29 @@ export default class Index extends Component {
   }
 
   takePhoto(){
-    Taro.navigateTo({url: '/pages/index/arrive/arrive'})
+    console.log('state',this.state);
+  }
+
+  upInfo(){
+    this.state.heavy.push({
+      pic0:this.state.defaultImg,
+      pic1:this.state.defaultImg,
+      pic2:this.state.defaultImg,
+      pic3:this.state.defaultImg
+    })
+    this.state.empty.push({
+      pic0:this.state.defaultImg,
+      pic1:this.state.defaultImg,
+      pic2:this.state.defaultImg,
+      pic3:this.state.defaultImg
+    })
+    this.setState({
+      heavy:this.state.heavy,
+      empty:this.state.empty,
+      isArr:false,
+      isEmp: true,
+      picGroup:this.state.picGroup+1
+    })
   }
 
   render () {
@@ -296,10 +382,10 @@ export default class Index extends Component {
             <Button className='account__myButton' onClick={this.takePhoto} disabled={isArr} type='primary'>重车到货</Button>
           </View>
           <View className="zan-cell-btn">
-            <button className='account__myButton' disabled={isEmp} type='primary'>空车拍照</button>
+            <Button className='account__myButton' onClick={this.takePhoto} disabled={isEmp} type='primary'>空车拍照</Button>
           </View>
           <View className="zan-cell-btn">
-            <button className='account__myButton' type='primary'>多点卸载</button>
+            <Button className='account__myButton' onClick={this.upInfo}  disabled={!(isArr && isEmp)} type='primary'>多点卸载</Button>
           </View>
           <View className="zan-cell-btn">
             <button className='account__myButton' type='default'>订单完成</button>
