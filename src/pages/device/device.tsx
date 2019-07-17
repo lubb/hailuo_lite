b3ce36a0ae24f0e1b8d957f1163f7c826e292a50
lubingbing <414107871@qq.com>
2019-07-17 10:36:57 +0800

    设备列表问题

--- src/pages/device/device.tsx
+++ src/pages/device/device.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Image, Text, Input, Button  } from '@tarojs/components'
 import './device.scss'
+import ajax from "../../common/js/ajax";
 
 export default class Device extends Component {
 
@@ -20,16 +21,14 @@ export default class Device extends Component {
     this.state = {
       latitude:'29.580554551939038',
       longitude:'111.27982597122477',
-      zzMsg:'获取中...',
-      sbztMsg:'获取中...',
       markers: [{
         id: 0,
         iconPath: require("./../../common/images/track.png"),
-        latitude: 29.580554551939038,
+        latitude: '29.580554551939038',
         longitude: '111.27982597122477',
         title: '安徽中凯信息股份有限公司',
         callout: {
-          content: '湘J68035' + '\n' + 'GPS状态：离线\n' + '载重传感：载重正常',
+          content: '湘J68035' + '\n' + 'GPS状态：离线\n' + '载重：XX顿\n速度：0',
           display: 'ALWAYS',
           padding: 10
         }
@@ -37,7 +36,10 @@ export default class Device extends Component {
     }
   }
 
-  componentWillMount () { }
+  componentWillMount () {
+   this.getCurrentCarInfo();
+   this.inter_data();
+  }
 
   componentDidMount () { }
 
@@ -47,8 +49,61 @@ export default class Device extends Component {
 
   componentDidHide () { }
 
+  dataInit(carId, token){
+    let data = 'carIds='+carId;
+    ajax.postToken("/api/car/selectCurSites",data,'application/x-www-form-urlencoded', token).then(r=>{
+      Taro.hideLoading();
+      console.log(r);
+      let d = r.data.bizContent;
+      if(d && d.length>0){
+        let d_ = d[0];
+        this.setState({
+          latitude: d_.lon,
+          longitude:d_.lat,
+          markers: [{
+            id: 0,
+            iconPath: require("./../../common/images/track.png"),
+            latitude: d_.lon,
+            longitude: d_.lat,
+            title: '安徽中凯信息股份有限公司',
+            callout: {
+              content: d_.carNumber + '\n' + 'GPS状态：'+(d_.gpsType===1?'在线':'静止中')+'\n' + '载重：'+d_.weight+'顿\n速度：'+d_.speed,
+              display: 'ALWAYS',
+              padding: 10
+            }
+          }]
+        })
+      }
+
+    });
+  }
+
+  /**
+   * 获取当前用户的车信息
+   */
+  getCurrentCarInfo(){
+    let token = Taro.getStorageSync('token');
+    Taro.showLoading({
+      title: '加载中',
+    });
+    ajax.postToken("/api/car/driverCar",'','application/x-www-form-urlencoded', token).then(r=>{
+      let carInfo = r.data.bizContent;
+      console.log('carInfo',carInfo);
+      Taro.hideLoading();
+      this.dataInit(carInfo.id, token);
+    });
+  }
+
+  inter_data(){
+    let that = this;
+    setInterval(function () {
+      // console.log('执行了！')
+      that.getCurrentCarInfo();
+    }, 10000);
+  }
+
   render () {
-    const {latitude,longitude,markers,sbztMsg,zzMsg} = this.state;
+    const {latitude,longitude,markers} = this.state;
     return (
       <View className="page-body">
         <View className="page-section page-section-gap">
