90954b3be530d6a9be0af6c51487a2faca346e9b
lubingbing <414107871@qq.com>
2019-07-19 10:44:21 +0800

    修改生产地址

--- src/config-server/hailuoApi.js
+++ src/config-server/hailuoApi.js
@@ -8,8 +8,8 @@
  * 测试环境
  */
 const UrlConstantsUat = {
-  hailuo_api_path: 'http://119.23.144.116:9700',
-  hailuo_service_path: 'http://119.23.144.116:9720',
+  hailuo_api_path: 'https://auth.hailuo.ah-zk.com',
+  hailuo_service_path: 'https://api.hailuo.ah-zk.com',
   hailuo_img_path:'http://img.hailuo.ah-zk.com',
   hailuo_img_upload:'https://fs.hailuo.ah-zk.com/upload',
   appId: 'wx55d3f914106096d8',
